c585efa5392de606d07bba6eca9425362bba34fb
shrijo <jonas.shriqui@gmail.com>
2024-07-26 16:38:52 +0200

    Update Navigation.tsx

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -4,17 +4,14 @@ import "./Navigation.css";
 
 async function Navigation() {
   const client = createClient();
-  const settings = await client.getSingle("settings"); // UPDATE THE UID TO MATCH YOURS
+  const settings = await client.getSingle("settings");
 
   return (
     <nav>
       <p className="logo">{settings.data.title}</p>
       <div className="nav-links">
         {settings.data.navigation.map((item) => (
-          <PrismicNextLink
-            key={item.navigation_link_title}
-            field={item.navigation_link}
-          >
+          <PrismicNextLink field={item.navigation_link}>
             <p>{item.navigation_link_title}</p>
           </PrismicNextLink>
         ))}
