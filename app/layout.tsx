a3ee72218ccfeb897bc67c1078f6009cc825a99f
shrijo <jonas.shriqui@gmail.com>
2024-07-26 15:59:02 +0200

    removed nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
         <div className="container">{children}</div>
       </body>
     </html>
