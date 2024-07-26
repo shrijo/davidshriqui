5144815524346fee6b6bb86f5b3cdbb5df711045
shrijo <jonas.shriqui@gmail.com>
2024-07-26 17:02:46 +0200

    nav

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { PrismicNextLink } from "@prismicio/next";
+import { createClient } from "@/prismicio";
+import "./Navigation.css";
 
-function Navigation() {
-  return <div>hello</div>;
+async function Navigation() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return (
+    <nav>
+      <p className="logo">{settings.data.title}</p>
+      <div className="nav-links">
+        {settings.data.navigation.map((item) => (
+          <PrismicNextLink
+            key={item.navigation_link_title}
+            field={item.navigation_link}
+          >
+            <p>{item.navigation_link_title}</p>
+          </PrismicNextLink>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
