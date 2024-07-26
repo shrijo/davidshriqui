00290d0d068c1f6195be2dc2e44716f679d83abf
shrijo <jonas.shriqui@gmail.com>
2024-07-26 16:27:37 +0200

    Update Navigation.tsx

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -11,12 +11,9 @@ async function Navigation() {
       <p className="logo">{settings.data.title}</p>
       <div className="nav-links">
         {settings.data.navigation.map((item) => (
-          <PrismicNextLink
-            key={item.navigation_link_title}
-            field={item.navigation_link}
-          >
+          <a key={item.navigation_link_title} field={item.navigation_link}>
             <p>{item.navigation_link_title}</p>
-          </PrismicNextLink>
+          </a>
         ))}
       </div>
     </nav>
