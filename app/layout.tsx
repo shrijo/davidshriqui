4d246d4e7f0532547b0e00ed86811d35c4ba2d74
shrijo <jonas.shriqui@gmail.com>
2024-07-26 16:14:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navigation from "./components/Navigation/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navigation />
         <div className="container">{children}</div>
       </body>
     </html>
