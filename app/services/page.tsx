5b95ed77759c6b923285b936bf433cddddbaf6b7
shrijo <jonas.shriqui@gmail.com>
2024-07-31 11:03:55 +0200

    services bullets

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/prismicio";
 import { PrismicRichText } from "@prismicio/react";
+import "./page.css";
 
 export default async function Services() {
   const client = createClient();
@@ -9,15 +10,13 @@ export default async function Services() {
     <div>
       <h1>{content.data.page_title}</h1>
       <p className="lead-text">{content.data.page_lead}</p>
-      <ul>
-        {services.map((service) => (
-          <li key={service.id}>
-            <h2>{service.data.service_title}</h2>
-            <PrismicRichText field={service.data.service_description} />
-            {/* <PrismicRichText field={service.data.service_description} /> */}
-          </li>
-        ))}
-      </ul>
+      {services.map((service) => (
+        <div className="service-block" key={service.id}>
+          <h2>{service.data.service_title}</h2>
+          <PrismicRichText field={service.data.service_description} />
+          {/* <PrismicRichText field={service.data.service_description} /> */}
+        </div>
+      ))}
     </div>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicRichText } from "@prismicio/react";
 
+import "./page.css";
+
 export default async function Services() {
   const client = createClient();
   const content = await client.getSingle("work");
@@ -8,15 +10,13 @@ export default async function Services() {
     <div>
       <h1>{content.data.page_title}</h1>
       <p className="lead-text">{content.data.page_description}</p>
-      <ul>
-        {content.data.projects.map((project) => (
-          <li key={project.project_title}>
-            <h2>{project.project_title}</h2>
-            <PrismicRichText field={project.project_description} />
-            {/* <PrismicRichText field={service.data.service_description} /> */}
-          </li>
-        ))}
-      </ul>
+      {content.data.projects.map((project) => (
+        <div className="work-box" key={project.project_title}>
+          <h2>{project.project_title}</h2>
+          <PrismicRichText field={project.project_description} />
+          {/* <PrismicRichText field={service.data.service_description} /> */}
+        </div>
+      ))}
     </div>
   );
 }
